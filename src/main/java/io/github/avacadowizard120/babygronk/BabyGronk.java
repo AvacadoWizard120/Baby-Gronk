e4fd06f22c35cac541d15fa93e3d45129971b178
ConfusingFool93 <edenjudeurias@gmail.com>
2024-04-11 10:52:58 -0500

    Added logic

--- src/main/java/io/github/avacadowizard120/babygronk/BabyGronk.java
+++ src/main/java/io/github/avacadowizard120/babygronk/BabyGronk.java
@@ -1,13 +1,32 @@
 package io.github.avacadowizard120.babygronk;
 
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BabyGronk extends JavaPlugin {
+public final class BabyGronk extends JavaPlugin implements Listener {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
+    public void onEnable()
+    {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event)
+    {
+        if (event.getEntityType() == EntityType.ZOMBIE)
+        {
+            Zombie zombie = (Zombie) event.getEntity();
+            if (zombie.isBaby())
+            {
+                zombie.setCustomName("Baby Gronk");
+                zombie.setCustomNameVisible(true);
+            }
+        }
     }
 
     @Override
